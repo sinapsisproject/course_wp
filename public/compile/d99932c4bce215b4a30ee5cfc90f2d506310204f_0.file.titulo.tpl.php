306fb792e697df51cc1d92b576dba3549f984904
Diego Baeza <diego.icinf@gmail.com>
2024-03-30 02:24:19 -0300

    agregacion de funcion para transaccion

--- public/compile/d99932c4bce215b4a30ee5cfc90f2d506310204f_0.file.titulo.tpl.php
+++ public/compile/d99932c4bce215b4a30ee5cfc90f2d506310204f_0.file.titulo.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.4.1, created on 2024-03-13 16:42:39
+/* Smarty version 4.4.1, created on 2024-03-30 05:18:16
   from 'C:\wamp64\www\sinapsis\wp-content\plugins\course_wp\public\partials\course\titulo.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.4.1',
-  'unifunc' => 'content_65f1d77fc81c12_71510195',
+  'unifunc' => 'content_6607a0985ee885_88047176',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'd99932c4bce215b4a30ee5cfc90f2d506310204f' => 
     array (
       0 => 'C:\\wamp64\\www\\sinapsis\\wp-content\\plugins\\course_wp\\public\\partials\\course\\titulo.tpl',
-      1 => 1710347864,
+      1 => 1711775766,
       2 => 'file',
     ),
   ),
@@ -20,16 +20,11 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_65f1d77fc81c12_71510195 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6607a0985ee885_88047176 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="row mt-5 mb-5">
     <div class="col-9">
         <h1 class="title-course-free"><?php echo $_smarty_tpl->tpl_vars['curso']->value->nombre;?>
 </h1>
-    </div>
-    <div class="col-12 d-inline-flex">
-        <button type="button" class="btn">Me quiero inscribir <i style="margin-left: 10px;" class="fa-solid fa-arrow-right"></i></button>
-        <h3 class="title-price">$ <?php echo $_smarty_tpl->tpl_vars['curso']->value->precio;?>
-</h3>
     </div>
 </div><?php }
 }
--- public/partials/course/titulo.tpl
+++ public/partials/course/titulo.tpl
@@ -2,8 +2,4 @@
     <div class="col-9">
         <h1 class="title-course-free">{$curso->nombre}</h1>
     </div>
-    <div class="col-12 d-inline-flex">
-        <button onclick="create_transaction({$id_curso})" type="button" class="btn">Me quiero inscribir <i style="margin-left: 10px;" class="fa-solid fa-arrow-right"></i></button>
-        <h3 class="title-price">$ {$curso->precio|number_format:0:',':'.'}</h3>
-    </div>
 </div>
