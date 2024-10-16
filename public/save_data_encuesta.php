9aaa1f435ff993ac1b5367159ba0c1daa5d21d8d
sinapsisproject <159088636+sinapsisproject@users.noreply.github.com>
2024-10-16 18:35:12 -0300

    Update save_data_encuesta.php

--- public/save_data_encuesta.php
+++ public/save_data_encuesta.php
@@ -3,35 +3,28 @@
     require(dirname(__FILE__) .'/../../../../wp-load.php');
 
     $respuestas = $_POST["respuestas"];
-    $id_encuesta = $_POST["id_encuesta"];
+
     $user_id = get_current_user_id();
-    $token = get_user_meta($user_id, 'tokensinapsisplatform', true);
+    $token   = get_user_meta($user_id, 'tokensinapsisplatform', true);
 
-    // Verifica si ya existen respuestas para este usuario y encuesta.
-    $existe_respuesta = RfCoreCurl::curl(
-        "/api/encuesta/verificar_respuesta?usuario=$user_id&encuesta=$id_encuesta",
-        'GET',
-        $token
-    );
+    $body = [
+        "respuestas" => $respuestas
+    ];
+    
+    $response = RfCoreCurl::curl('/api/encuesta/insert_response_formularios' , 'POST' , $token, $body);
 
-    if ($existe_respuesta->status == true) {
+
+    if($response->status == true){
         wp_send_json(array(
-            'status' => false,
-            'response' => 'Ya has enviado respuestas para esta encuesta.'
-    ));
-    exit;
+            'status'   => true,
+            'response' => $response->response
+        ));
+    }else{
+        wp_send_json(array(
+            'status'    => false,
+            'response'  => $response->response
+        ));
     }
 
-    // Si no existen respuestas previas, guarda las nuevas.
-    $body = ["respuestas" => $respuestas];
-    $response = RfCoreCurl::curl('/api/encuesta/insert_response_formularios', 'POST', $token, $body);
-
-    if ($response->status == true) {
-    wp_send_json(array('status' => true, 'response' => $response->response));
-    } else {
-    wp_send_json(array('status' => false, 'response' => $response->response));
-    }
 
 ?>
-
-
