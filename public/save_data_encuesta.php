dea1d32c471aa3111eb24775523e5dd98eebf01e
sinapsisproject <159088636+sinapsisproject@users.noreply.github.com>
2024-10-16 17:56:30 -0300

    Update save_data_encuesta.php

--- public/save_data_encuesta.php
+++ public/save_data_encuesta.php
@@ -1,44 +1,32 @@
 <?php 
 
-require(dirname(__FILE__) . '/../../../../wp-load.php');
+    require(dirname(__FILE__) .'/../../../../wp-load.php');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    guardar_respuestas();
-} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    obtener_respuestas();
-}
-
-// Función para guardar las respuestas.
-function guardar_respuestas() {
     $respuestas = $_POST["respuestas"];
+
     $user_id = get_current_user_id();
-    $token = get_user_meta($user_id, 'tokensinapsisplatform', true);
+    $token   = get_user_meta($user_id, 'tokensinapsisplatform', true);
 
-    $body = ["respuestas" => $respuestas];
-    $response = RfCoreCurl::curl('/api/encuesta/insert_response_formularios', 'POST', $token, $body);
+    $body = [
+        "respuestas" => $respuestas
+    ];
+    
+    $response = RfCoreCurl::curl('/api/encuesta/insert_response_formularios' , 'POST' , $token, $body);
 
-    if ($response->status == true) {
-        wp_send_json(['status' => true, 'response' => $response->response]);
-    } else {
-        wp_send_json(['status' => false, 'response' => $response->response]);
+
+    if($response->status == true){
+        wp_send_json(array(
+            'status'   => true,
+            'response' => $response->response
+        ));
+    }else{
+        wp_send_json(array(
+            'status'    => false,
+            'response'  => $response->response
+        ));
     }
-}
-
-// Función para obtener las respuestas guardadas.
-function obtener_respuestas() {
-    $id_encuesta = $_GET['encuesta'];
-    $user_id = get_current_user_id();
-    $token = get_user_meta($user_id, 'tokensinapsisplatform', true);
-
-    $body = ["id_encuesta" => $id_encuesta];
-    $response = RfCoreCurl::curl('/api/encuesta/get_user_responses', 'POST', $token, $body);
-
-    if ($response->status == true) {
-        wp_send_json(['status' => true, 'respuestas' => $response->respuestas]);
-    } else {
-        wp_send_json(['status' => false, 'message' => 'No se encontraron respuestas.']);
-    }
-}
 
 
 ?>
+
+
